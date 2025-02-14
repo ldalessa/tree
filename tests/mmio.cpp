72590782a5c4b5a025b9d4514cabc03201b61073
Luke D'Alessandro <ldalessa@uw.edu>
2025-02-13 16:19:00 -0800

    Deal with some errors in how services were selected.

--- tests/mmio.cpp
+++ tests/mmio.cpp
@@ -16,32 +16,32 @@ auto main(int argc, char** argv) -> int
 	CLI::App app;
 	options::process_command_line(app);
 
-	int n_threads = 2;
+	unsigned n_consumers = 1;
 	unsigned n_services = 1;
 	unsigned n_edges = -1;
 	std::string path{};
 
 	app.add_option("path", path, "The path to the mmio file")->required();
 	app.add_option("n_edges", n_edges, "The number of edges to process (default: all)");
-	app.add_option("-t, --n_threads", n_threads, std::format("The number of threads to use (default: {})", n_threads));
+	app.add_option("-c, --n_consumers", n_consumers, std::format("The number of threads to use as consumers (default: {})", n_consumers));
 	app.add_option("-n, --n_services", n_services, "The number of services to provision (default: 1)");
 	
 	CLI11_PARSE(app, argc, app.ensure_utf8(argv));
 	
-	assert(2 <= n_threads);
+	assert(1 <= n_consumers);
 	assert(1 <= n_services);
 
-	std::print("n_threads: {}\nn_services: {}\nn_edges: {}\n", n_threads, n_services, n_edges);	
+	std::print("n_consumers: {}\nn_services: {}\nn_edges: {}\n", n_consumers, n_services, n_edges);	
 	std::fflush(stdout);
 	
 	auto const mm = mmio::MatrixMarketFile(path);
 
 	auto tree = TreeNode<ValueNode<unsigned>>("0/0");
-	auto queues = std::vector<SPSCQueue<Key, 512>>(n_threads - 1);
+	auto queues = std::vector<SPSCQueue<Key, 512>>(n_consumers);
 	auto done = std::atomic_flag(false);
 	auto consumers = std::vector<std::jthread>();
 
-	for (int i = 0; i < n_threads - 1; ++i) {
+	for (int i = 0; i < n_consumers; ++i) {
 		consumers.emplace_back([i,&tree,&queues,&done] {
 			unsigned n = 0;
 			while (not done.test()) {
@@ -52,15 +52,18 @@ auto main(int argc, char** argv) -> int
 			while (auto key = queues[i].pop()) {
 				tree.insert(*key, n++);
 			}
+
+			std::print("consumer {} processed {} keys\n", i, n);
 		});
 	}
 	
-	int n_service_shift = std::countl_zero((u64)std::bit_ceil(n_services));
+	int n_consumer_shift = std::countl_zero((u64)std::bit_ceil(n_consumers)) + 1;
 	int n = 0;
+	std::print("consumer shift: {}\n", n_consumer_shift);
 	for (auto [u, v] : edges(mm)) {
 		if (n++ < n_edges) {
-			Key key(u,v);
-			auto service = u >> n_service_shift;
+			auto const key = Key(u,v);
+			auto const service = std::min((n_consumer_shift < 64) ? std::byteswap((u64)v) >> n_consumer_shift : 0, (u64)n_consumers - 1);
 			while (not queues[service].push(key)) {
 			}
 		}
@@ -73,7 +76,8 @@ auto main(int argc, char** argv) -> int
 	
 	for (unsigned n = 0; auto [u, v] : edges(mm)) {
 		if (n++ < n_edges) {
-			auto const node = tree.find(Key(u, v));
+			auto const key = Key(u, v);
+			auto const node = tree.find(key);
 			assert(node != nullptr);
 			assert(node->get_value());
 		}
